2a38b552d2590424c4222b2b23fc93ecf8e84203
xiaoluoboding <xiaoluoboding@gmail.com>
2021-08-10 15:03:56 +0800

    refactor: rename repo name in console

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,9 +1,9 @@
 /* eslint-disable no-console */
 import { onMessage } from 'webext-bridge'
 
-console.info('[vitesse-webext] Hello world from content script')
+console.info('[vitesse-modernized-chrome-ext] Hello world from content script')
 
 // communication example: send previous tab title from background page
 onMessage('tab-prev', ({ data }) => {
-  console.log(`[vitesse-webext] Navigate from page "${data.title}"`)
+  console.log(`[vitesse-modernized-chrome-ext] Navigate from page "${data.title}"`)
 })
