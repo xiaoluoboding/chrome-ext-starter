bda5b61b079ca3ea0c07ebc04532260b77223de5
Yunwei Xiao <xiaoluoboding@gmail.com>
2022-04-24 11:30:17 +0800

    fix: fixed the issue #3

--- scripts/prepare-prod.ts
+++ scripts/prepare-prod.ts
@@ -25,6 +25,7 @@ async function stubIndexHtml() {
 }
 
 export async function writeManifest() {
+  await fs.ensureFile(r('extension/prod/manifest.json'))
   await fs.writeJSON(r('extension/prod/manifest.json'), await getManifest(), { spaces: 2 })
   logger('PRE', 'write manifest.json')
 }
